28f7ba7b81f445b8d275f8463bec3559ac3d95fa
Mariusz Kierski <mariusz.kierski@gmail.com>
2014-09-11 19:33:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+MongoPi Install Script - Usage
+======================
+
+Script and files needed to painlessly install MongoDB on a Raspberry Pi.
+
+To install, connect to a Pi and execute:
+`curl https://raw.githubusercontent.com/chaser92/mongopi-install-script/master/install.sh | bash`
+
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Based on tutorial from https://emersonveenstra.net/mongodb-raspberry-pi/
+echo Installing MongoDB...
+cd /tmp &&
+curl https://raw.githubusercontent.com/chaser92/mongopi-install-script/master/mongo.tar.gz | tar zx &&
+cp -r mongo/* /usr/ &&
+rm -r mongo &&
+adduser --firstuid 100 --ingroup nogroup --shell /etc/false --disabled-password --gecos "" --no-create-home mongodb &&
+mkdir -p /var/log/mongodb/ &&
+mkdir -p /var/lib/mongodb/ &&
+chown mongodb:nogroup /var/log/mongodb/ &&
+chown mongodb:nogroup /var/lib/mongodb/ &&
+curl -o /etc/init.d/mongod https://raw.githubusercontent.com/chaser92/mongopi-install-script/master/mongod &&
+curl -o /etc/mongodb.conf https://raw.githubusercontent.com/chaser92/mongopi-install-script/master/mongodb.conf &&
+/etc/init.d/mongod start &&
+echo MongoDB has been installed succesfully!
